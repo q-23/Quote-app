e32f37e029346c7f4fe5db9e9795b4f3eb70f240
Aleksander Kwiatkowski <aleksander.kwiatkowski@gmail.com>
2018-04-30 18:27:24 +0200

    Add getJoke function and two variables linking DOM tree

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
+var url = 'http://api.icndb.com/jokes/random';
+
+var $button = $('#get-joke').click(function() {
+	getJoke();
+});
+
+var $paragraph = $('#joke');
+
+function getJoke() {
+$.ajax({
+	method: 'GET',
+	url: url, //here's a weird construction, but on the left we have the name of the parameter, and to the right is the name of the variable that holds the value
+	success: function(res) {
+		$paragraph.text(res.value.joke);
+	}
+
+});
+}
+
 var tweetLink = "https://twitter.com/intent/tweet?text=";
 var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";s
