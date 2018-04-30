0e462279f92652bcb83bf168012b54848ddc8f86
Aleksander Kwiatkowski <aleksander.kwiatkowski@gmail.com>
2018-04-30 18:30:14 +0200

    Add getQuote function

--- script.js
+++ script.js
@@ -18,4 +18,8 @@ $.ajax({
 }
 
 var tweetLink = "https://twitter.com/intent/tweet?text=";
-var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";s
+var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1"
+
+function getQuote() {
+    $.getJSON(quoteUrl, createTweet);
+}
