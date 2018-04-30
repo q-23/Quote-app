154d5c9cac2a69dd099d172f204b1c591a1cdfd7
Aleksander Kwiatkowski <aleksander.kwiatkowski@gmail.com>
2018-05-01 00:59:20 +0200

    add styling and finish project

--- script.js
+++ script.js
@@ -1,24 +1,4 @@
 var prefix = "https://cors-anywhere.herokuapp.com/";
-
-var url = 'http://api.icndb.com/jokes/random';
-
-var $button = $('#get-joke').click(function() {
-	getJoke();
-});
-
-var $paragraph = $('#joke');
-
-function getJoke() {
-$.ajax({
-	method: 'GET',
-	url: url, //here's a weird construction, but on the left we have the name of the parameter, and to the right is the name of the variable that holds the value
-	success: function(res) {
-		$paragraph.text(res.value.joke);
-	}
-
-});
-}
-
 var tweetLink = "https://twitter.com/intent/tweet?text=";
 var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1"
 
